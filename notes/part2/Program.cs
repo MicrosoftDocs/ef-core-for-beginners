d2f8be4af7818ea59c1cd3d7a8986567d1367d13
Cam Soper <cam.soper@microsoft.com>
2022-04-20 01:49:19 +0000

    part2 wip

---
+++ notes/part2/Program.cs
@@ -0,0 +1,9 @@
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+foreach (Customer c in context.Customers)
+{
+    Console.WriteLine($"Name: {c.FirstLast}");
+}
